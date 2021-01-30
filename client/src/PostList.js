9d38667958f3a8bd001597540f97792a6319df19
Aseem Pandita <aseempandita@live.com>
2021-01-30 12:29:31 +0530

    Get data from Query service - reduce n/w calls.

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -9,8 +9,8 @@ export default () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
-
+        const res = await axios.get('http://localhost:4002/posts');
+        //console.log(res.data);
         setPosts(res.data);
     }; 
 
@@ -25,7 +25,7 @@ export default () => {
                 style={{border: 'solid 2px', padding: '5px', marginBottom: '5px'}}>
                     <div className="card-body">
                         <h3>{post.title}</h3>
-                        <CommentList postId={post.id}/>
+                        <CommentList comments={post.comments}/>
                         <CommentCreate postId={post.id}/>
                     </div>
                     
