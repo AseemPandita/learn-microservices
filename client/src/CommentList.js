2fde5a30a8ed219bc4c7c37549d9164b55075d7b
Aseem Pandita <aseempandita@live.com>
2021-01-30 22:04:09 +0530

    bug fixes

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -5,7 +5,21 @@ import React from 'react';
 export default ({comments}) => {
     
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content;
+
+        if (comment.status === 'approved') {
+            content = comment.content;
+        }
+
+        if (comment.status === 'rejected') {
+            content = 'Comment rejected by moderator.';
+        }
+
+        if (comment.status === 'pending') {
+            content = 'Comment awaiting approval by moderator.';
+        }
+
+        return <li key={comment.id}>{content}</li>
     });
 
     return <div>
